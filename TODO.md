390540f03da9f389bddb067e287d962b0114f418
Javi <javi@jsrois.net>
2021-09-09 16:47:46 +0200

    Add delete method

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 ## Pasos a seguir
 
-- [ ] Qué `endpoints` tiene el backend? 
+- [ ] Qué `endpoints` tiene el backend? Testar usando Postman.
 
 Ejemplo:
 
@@ -9,4 +9,6 @@ Ejemplo:
 | PUT | `/books/{i}` | editar un libro |
 
 
--[ ] Cómo podemos mostrar las cookies en el frontend?
+- [ ] Cómo podemos mostrar las Cookies en la app de frontend
+- [ ] Cómo podemos crear una nueva Cookie usando la app de frontend
+- [ ] Como podemos editar y borrar una cookie existente usando la app de frontend?
--- src/main/java/org/example/exampleapp/controllers/CookieController.java
+++ src/main/java/org/example/exampleapp/controllers/CookieController.java
@@ -3,12 +3,11 @@ package org.example.exampleapp.controllers;
 import org.example.exampleapp.entities.Cookie;
 import org.example.exampleapp.repositories.CookieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CookieController {
@@ -28,5 +27,11 @@ public class CookieController {
     @PostMapping("/cookies")
     public Cookie saveCookie(@RequestBody Cookie cookie) {
         return cookieRepository.save(cookie);
+    }
+
+    @DeleteMapping("/cookies/{id}")
+    public ResponseEntity<String> deleteCookie(@PathVariable Long id) {
+        cookieRepository.deleteById(id);
+        return ResponseEntity.of(Optional.of("eliminada cookie"));
     }
 }
