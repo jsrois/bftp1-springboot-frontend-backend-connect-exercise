f3dca7bdc5bb78a63d797920c75a4f257353d411
Javi <javi@jsrois.net>
2021-09-10 08:03:56 +0200

    Remove crossorigin annotation from controllers

--- src/main/java/org/example/exampleapp/controllers/CookieController.java
+++ src/main/java/org/example/exampleapp/controllers/CookieController.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin
 public class CookieController {
 
     private CookieRepository cookieRepository;
--- src/main/java/org/example/exampleapp/controllers/CookieTypeController.java
+++ src/main/java/org/example/exampleapp/controllers/CookieTypeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin
 public class CookieTypeController {
 
     @GetMapping("/cookies/types")
